1a5a446e8b2309bf37827dfcdda8a4b91446854e
xelahaippa <xelahaippa@users.noreply.github.com>
2021-02-02 16:05:14 +0000

    404 not found

--- app.js
+++ app.js
@@ -39,7 +39,8 @@ mongoose.set('useFindAndModify', false);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  // next(createError(404));
+        res.status(404).send("Page Not Found");
 });
 
 // error handler
