2e63b033075eac7a3bfa88401eba9f9d7d8fda07
xelahaippa <xelahaippa@users.noreply.github.com>
2021-02-03 05:53:29 +0000

    update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compression = require('compression');
 const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
@@ -16,7 +17,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
